0c27b100d7e86c30794d2de0e37329acb5019c58
Hyerim <hyyrim@gmail.com>
2021-11-09 05:41:05 +0900

    Visual

--- js/main.js
+++ js/main.js
@@ -16,3 +16,29 @@ searchInputEl.addEventListener('blur', () => {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle( () => {
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300 ));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach( (fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1 ) * .7,
+    opacity: 1
+  })
+});
