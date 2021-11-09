b1de3d11970de1fa19202be8f9a3935ba8a56ae5
Hyerim <hyyrim@gmail.com>
2021-11-09 12:47:07 +0900

    Promotion

--- js/main.js
+++ js/main.js
@@ -54,3 +54,36 @@ new Swiper('.notice-line .swiper-container', {
   loop: true
 });
 
+// 프로모션 슬라이더
+new Swiper('.promotion .swiper-container', {
+  slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기  
+  loop: true,
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자 
+    clickable: true // 사용자 페이지 번호 요소 제어 가능 여부
+  }, 
+  navigation: {
+    prevEl: '.promotion .swiper-prev',
+    nextEl: '.promotion .swiper-next'
+  } 
+});
+
+
+// 프로모션 토글
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', () => {
+  isHidePromotion = !isHidePromotion;
+  if (isHidePromotion) {
+    promotionEl.classList.add('hide');
+  } else {
+    promotionEl.classList.remove('hide');
+  }
+});
